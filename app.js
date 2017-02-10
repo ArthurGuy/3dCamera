7b8033ded666fa51baa6b567713da1d4f7bad330
Arthur Guy <arthur@arthurguy.co.uk>
2017-02-10 20:09:46 +0000

    stop when the image is taken

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ var camera = new RaspiCam({ mode: 'photo', output: '/home/pi/3dCamera/images/%d.
 
 camera.start({timeout: 0});
 
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, timestamp, filename){ 
+    camera.stop();
+});
