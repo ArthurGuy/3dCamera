ff9ee1030e1ce000e9be86fcaeea33013c7ec0f5
Arthur Guy <arthur@arthurguy.co.uk>
2017-02-10 20:50:07 +0000

    do we need stop?

--- app.js
+++ app.js
@@ -10,18 +10,25 @@ camera.on("started", function(){
     console.log("started")
 });
 
-//listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){
-    console.log("new file available", timestamp, filename);
-    camera.stop();
-});
 
-socket.on('connect', function(){
-    console.log('A socket connection was made');
-});
-  
+
 socket.on('client-count', function(data){
     console.log("Taking a photo");
     camera.set('output', '/home/pi/3dCamera/output1.jpg');
     camera.start({timeout: 0 });
 });
+
+
+
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, timestamp, filename){
+    console.log("new file available", timestamp, filename);
+//    camera.stop();
+});
+
+
+
+socket.on('connect', function(){
+    console.log('A socket connection was made');
+});
+  
