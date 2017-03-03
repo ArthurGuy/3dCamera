a16aafa9ea971f03a7ab9b90145bfcd8e5c428f1
Arthur Guy <arthur@arthurguy.co.uk>
2017-03-03 23:03:56 +0000

    delete the file when its sent

--- app.js
+++ app.js
@@ -39,33 +39,6 @@ socket.on('take-photo', function(data){
     
 });
 
-/*
-
-//listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, timestamp, filename){
-    console.log("new file available", timestamp, filename);
-    camera.stop();
-    var filePath = __dirname + '/output.jpg';
-    
-    //var imageData = fs.readFileSync(filePath,'utf8');
-    //console.log(imageData.toString('base64'));
-    //socket.emit('new-photo', {data:imageData.toString('base64')});
-    
-    fs.readFile(filePath, function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        //console.log(data.toString('base64'));
-        console.log("Sending photo");
-        socket.emit('new-photo', {data:data.toString('base64'), takeId:takeId, startTime:lastReceiveTime, time:Date.now()});
-        
-        console.log("Deleting photo");
-        fs.unlink(filePath);
-    });
- 
-});
-*/
-
 
 socket.on('connect', function(){
     console.log('A socket connection was made');
@@ -118,6 +91,11 @@ function sendImage() {
             photoStartTime:photoStartTime,
             totalDelay: totalDelay,
             imageDelay: imageDelay
+        });
+        
+        // Remove the image
+        fs.unlink(getAbsoluteImagePath(), function () {
+            // file deleted
         });
     });
 }
